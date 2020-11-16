5d6d2e4c3f4ecf044123f1afdd74e3ef2a6ae2fe
Iden Han <idenhan129@gmail.com>
2020-11-16 15:21:16 +0900

    Change and Implement user forms

--- src/form/AddUserForm.jsx
+++ src/form/AddUserForm.jsx
@@ -1,30 +1,30 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
 const AddUserForm = (props) => {
-  const initUser = { name: '', password: '', comment: ''};
+
+  const initUser = {id: null, name: '', username: ''};
 
   const [user, setUser] = useState(initUser);
 
   const handleChange = e => {
-    const { name, value } = e.target;
-    setUser({...user, [name] : value});
+      const {name, value} = e.target;
+      setUser({...user, [name]: value});
   }
 
   const handleSubmit = e => {
     e.preventDefault();
-    if (user.name && user.comment) {
+    if (user.name && user.username) {
       handleChange(e, props.addUser(user));
     }
   }
-
   return (
-    <form>
-      <label>Name</label>
-      <input className='u-full-width' type='text' name='name' value={user.name} onChange={handleChange}/>
-      <label>Comment</label>
-      <input className='u-full-width' type='text' name='comment' value={user.comment} onChange={handleChange}/>
-      <button className='button-primary' type='submit' onClick={handleSubmit}>Add user</button>
-    </form>
+      <form>
+          <label>Name</label>
+          <input className="u-full-width" type="text" value={user.name} name="name" onChange={handleChange} />
+          <label>Username</label>
+          <input className="u-full-width" type="text" value={user.username} name="username" onChange={handleChange} />
+          <button className="button-primary" type="submit" onClick={handleSubmit} >Add user</button>
+      </form>
   )
 }
 
