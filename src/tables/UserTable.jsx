a25a689e243a4151a7e4e4c948cee49c3a3fed9e
Iden Han <idenhan129@gmail.com>
2020-11-16 15:22:00 +0900

    Change and Fix errors on UserTable

--- src/tables/UserTable.jsx
+++ src/tables/UserTable.jsx
@@ -1,40 +1,41 @@
 import React from 'react';
-import { format } from 'date-fns';
 
-const userTable = (props) => {
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Comment</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        { props.users.length > 0 ? (
-          props.users.map(user => {
-            const {name, password, comment} = user;
-            return (
-              <tr>
-                <td>{name}</td>
-                <td>{comment}</td>
-                <td>{format(new Date(), 'yyyy-MM-dd')}</td>
-                <td>
-                  <button onClick={() => props.deleteUser(password)}>Delete</button>
-                  <button>Edit</button>
-                </td>
-              </tr>
-            )
-          })
-        ) : (
-          <tr>
-            <td colSpan={4}>No users found</td>
-          </tr>
-        )}
-      </tbody>
-    </table>
-  )
+const UserTable = (props) => {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Username</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                { props.users.length > 0 ? (
+                    props.users.map(user => {
+                        const {id, name, username} = user;
+                        return (
+                            <tr key={id}>
+                                <td>{id}</td>
+                                <td>{name}</td>
+                                <td>{username}</td>
+                                <td>
+                                    <button onClick={() => props.deleteUser(id)}>Delete</button>
+                                    <button onClick={() => props.editUser(id, user)}>Edit</button>
+                                </td>
+                            </tr>
+                        )
+                    })
+                ) : (
+                    <tr>
+                        <td colSpan={4}>No users found</td>
+                    </tr>
+                )   
+                }
+            </tbody>
+        </table>
+    )
 }
 
-export default userTable;
+export default UserTable;
